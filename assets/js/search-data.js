495af106f82993582b2c36b6e2b0609457a5f333
jackychen8173 <jchen8173@gmail.com>
2026-04-23 23:14:26 +0000

    Deploying to gh-pages from @ jackychen8173/jackychen8173.github.io@6420d7c70deb81f87cfcc2c09caaedfc31d735f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,31 +451,31 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-tutorspace",
+          title: 'TutorSpace',
+          description: "Full-stack tutoring management platform",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/1_tutorspace/";
+            },},{id: "projects-plant-tissue-growth-simulation",
+          title: 'Plant Tissue Growth Simulation',
+          description: "Computational biology research with auxin transport modeling",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/2_plant-simulation/";
+            },},{id: "projects-youth-data-explorer",
+          title: 'Youth Data Explorer',
+          description: "Interactive data visualization for GLOCAL Foundation",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/3_youth-data-explorer/";
+            },},{id: "projects-indoor-vpr-with-dinov2",
+          title: 'Indoor VPR with DINOv2',
+          description: "Visual place recognition using self-supervised learning",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/4_indoor-vpr/";
+            },},{id: "projects-bc-covid-19-dashboard",
+          title: 'BC COVID-19 Dashboard',
+          description: "Public health data visualization with Power BI",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/projects/5_covid-dashboard/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
